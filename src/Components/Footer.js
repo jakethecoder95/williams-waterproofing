22f7190451ea518d195ef69cc4354cdd7f62490f
Jacob Robinson <95jacob07@gmail.com>
2019-06-03 23:41:40 -0700

    footer added

---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = props => {
+  return (
+    <footer className="page-footer">
+      <p className="text-center">
+        © 2017-2018 Williams Waterproofing, Inc. - All Rights Reserved
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
